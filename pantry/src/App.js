f27542bf76cd2fd0ab3b2ee045a8d5a9aab372d1
Cody Raymond <codyraymond@rocketmail.com>
2021-08-17 10:07:02 -0400

    initial push to new branch

--- pantry/src/App.js
+++ pantry/src/App.js
@@ -4,8 +4,8 @@ import NavBar from './components/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      <div className="title">
+    <div className="App" >
+      <div className="title" style={{textDecoration: 'underline'}}>
         <h1>Title</h1>
       </div>
       <NavBar />
