d8dbfcf2beb09d83d93421b2abdfa93bbc796ef5
bryson crandall <bryson.crandall@yahoo.com>
2021-08-17 22:25:14 -0700

    changes to schema, refer to ERD

--- migrations/20210817123833_ingredients.js
+++ migrations/20210817123833_ingredients.js
@@ -1,15 +1,12 @@
 
-exports.up = function(knex) {
+exports.up = function (knex) {
     return knex.schema.createTable('ingredients', table => {
         table.increments('ingredient_id');
         table.string('name')
-        table.decimal('amount', 8, 2)
-        table.string('amount_unit')
         table.boolean('perishable')
-        table.date('expiration')
     });
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
     return knex.schema.dropTableIfExists('ingredients')
 };
